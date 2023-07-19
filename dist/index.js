52cb8570d59abf1cb8975987a339678e5ebfa859
Troncoso Juan <38189059+troncosojuan@users.noreply.github.com>
2023-07-19 13:01:02 -0300

    delete dist

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
-var _config = require("./config");
-var _app = _interopRequireDefault(require("./app"));
-require("./database.js");
-_app["default"].listen(_config.PORT);
-console.log("PORT: ", _config.PORT);
